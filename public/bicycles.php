7bb0021d5ed6d16a4ef7afea11a964325a48629a
aliahmadcse <aliahmadcse@gmail.com>
2020-03-19 23:14:22 +0500

    Updates initial files

--- public/bicycles.php
+++ public/bicycles.php
@@ -3,6 +3,7 @@
 <?php $page_title = 'Inventory'; ?>
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
+
 <div id="main">
 
   <div id="page">
@@ -25,28 +26,28 @@
         <th>Condition</th>
         <th>Price</th>
       </tr>
+      <?php
+      $parser = new ParseCSV(PRIVATE_PATH . '/used_bicycles.csv');
+      $bike_array = $parser->parse();
 
-<?php
+      // print_r($bike_array);
+      foreach ($bike_array as $args) {
+        // $args = ['brand' => 'Trek', 'model' => 'Emonda', 'year' => '2017'];
+        $bicylce = new Bicycle($args);
 
-$parser = new ParseCSV(PRIVATE_PATH . '/used_bicycles.csv');
-$bike_array = $parser->parse();
-
-?>
-      <?php foreach($bike_array as $args) { ?>
-        <?php $bike = new Bicycle($args); ?>
-      <tr>
-        <td><?php echo h($bike->brand); ?></td>
-        <td><?php echo h($bike->model); ?></td>
-        <td><?php echo h($bike->year); ?></td>
-        <td><?php echo h($bike->category); ?></td>
-        <td><?php echo h($bike->gender); ?></td>
-        <td><?php echo h($bike->color); ?></td>
-        <td><?php echo h($bike->weight_kg()) . ' / ' . h($bike->weight_lbs()); ?></td>
-        <td><?php echo h($bike->condition()); ?></td>
-        <td><?php echo h(money_format('$%i', $bike->price)); ?></td>
-      </tr>
+      ?>
+        <tr>
+          <td><?php echo h($bicylce->brand); ?></td>
+          <td><?php echo h($bicylce->model); ?></td>
+          <td><?php echo h($bicylce->year); ?></td>
+          <td><?php echo h($bicylce->category); ?></td>
+          <td><?php echo h($bicylce->gender); ?></td>
+          <td><?php echo h($bicylce->color); ?></td>
+          <td><?php echo h($bicylce->weight_kg()) . ' / ' . h($bicylce->weight_lbs()); ?></td>
+          <td><?php echo h($bicylce->condition()); ?></td>
+          <td><?php echo "$" . h(number_format($bicylce->price(), 2)); ?></td>
+        </tr>
       <?php } ?>
-
     </table>
   </div>
 
